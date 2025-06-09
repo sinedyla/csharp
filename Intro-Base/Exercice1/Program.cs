9b6ce5a67081b30514fec601036fd1ffb487fd22
Sinedyla <sinedyla@gmail.com>
2025-06-09 23:18:50 +0000

    Surface d'un triangle

---
+++ Intro-Base/Exercice1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Fonction pour calculer la surface d'un triangle
+
+using System;
+
+class Programme{
+    static double SurfaceTriangle(double baseTriangle, double hauteur){
+        return (baseTriangle * hauteur) / 2;
+    }
+
+    static void Main()
+    {
+        double baseT = 10;
+        double hauteur = 5;
+
+        double surface = SurfaceTriangle(baseT, hauteur);
+        Console.WriteLine("La surface du triangle est : " + surface);
+    }
+}
