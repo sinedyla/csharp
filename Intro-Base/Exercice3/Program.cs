b48467bbed8e689bac7eb550abbe1385ae6a9181
Sinedyla <sinedyla@gmail.com>
2025-06-09 23:24:10 +0000

    Le dernier element d'un tableau

---
+++ Intro-Base/Exercice3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Programme {
+    static int DernierElement(int[] tableau) {
+        if (tableau.Length == 0) {
+            throw new ArgumentException("Le tableau ne peut pas être vide.");
+        }
+        return tableau[tableau.Length - 1];
+    }
+    static void Main() {
+        int[] nombres = { 3, 7, 12, 9 };
+        int dernier = DernierElement(nombres);
+        Console.WriteLine("Le dernier élément du tableau est : " + dernier);
+    }
+}
