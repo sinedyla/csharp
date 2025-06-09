ef4f6be404c64b960e811170afb834a3e2da803e
Sinedyla <sinedyla@gmail.com>
2025-06-09 23:15:30 +0000

    Le premier programme

---
+++ Intro-Base/Exercice0/Program.cs
@@ -0,0 +1,10 @@
+﻿// Affiche le message "Bonjour le monde !"
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Bonjour le monde !");
+    }
+}
