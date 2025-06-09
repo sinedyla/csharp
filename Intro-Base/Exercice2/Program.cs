1a3a78724c49e2a462f5170c515d1a614f5daa6f
Sinedyla <sinedyla@gmail.com>
2025-06-09 23:21:10 +0000

    Le reste de la division

---
+++ Intro-Base/Exercice2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Fonction pour calculer le reste de la division de deux nombres
+
+using System;
+class Programme
+{
+    static int ResteDivision(int dividende, int diviseur){
+        return dividende % diviseur;
+    }
+    static void Main(){
+        int a = 17;
+        int b = 5;
+        int reste = ResteDivision(a, b);
+        Console.WriteLine("Le reste de la division de " + a + " par " + b + " est : " + reste);
+    }
+}
